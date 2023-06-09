e06ba772883eed62e506b3eb680f3244c55ac173
Shahil Mangroliya <60001265+ShahilMangroliya@users.noreply.github.com>
2023-06-09 16:43:34 +0530

    Update KCKeepAwake.m

--- ios/KCKeepAwake.m
+++ ios/KCKeepAwake.m
@@ -8,16 +8,16 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(activate)
 {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [[UIApplication sharedApplication] setIdleTimerDisabled:YES];
-    });
+//     dispatch_async(dispatch_get_main_queue(), ^{
+//         [[UIApplication sharedApplication] setIdleTimerDisabled:YES];
+//     });
 }
 
 RCT_EXPORT_METHOD(deactivate)
 {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [[UIApplication sharedApplication] setIdleTimerDisabled:NO];
-    });
+//     dispatch_async(dispatch_get_main_queue(), ^{
+//         [[UIApplication sharedApplication] setIdleTimerDisabled:NO];
+//     });
 }
 
 @end
